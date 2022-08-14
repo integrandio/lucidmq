b76463070e045b1ce730ceb8d3c2d05d1b3d75dd
Burak <burakkiran95@gmail.com>
2022-08-14 10:51:30 -0700

    add other files and new python release

--- node-lucidmq/test.mjs
+++ node-lucidmq/test.mjs
@@ -2,19 +2,20 @@ import { Message, LucidMQ } from './index.js'
 
 var maxSegmentSize = BigInt(1000n);
 var maxTopicSize = BigInt(100000n);
-const lucidmq = new LucidMQ("../xtestlog", maxSegmentSize, maxTopicSize);
+const lucidmq = new LucidMQ("../test_log", maxSegmentSize, maxTopicSize);
 
+console.log("program started");
 
-const buff = Buffer.from('hello');
-const mess = new Message(buff, buff);
-const producer = lucidmq.newProducer("topic1")
+// const buff = Buffer.from('hello');
+// const mess = new Message(buff, buff);
+// const producer = lucidmq.newProducer("topic1")
 
-producer.produceMessage(mess);
+// producer.produceMessage(mess);
 
-const consumer = lucidmq.newConsumer("topic1", "cg1")
+const consumer = lucidmq.newConsumer("topic1", "cg2")
 let messages = consumer.poll(maxSegmentSize);
 for (const message of messages) {
-    console.log(message.getKey().toString())
+    console.log(message.getKey().toString() + " : "+ message.getValue().toString());
 }
 
 
